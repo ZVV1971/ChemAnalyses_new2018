0c3683f9b40c084777dd1690d7bffad311939a17
ZVV1971 <vvzakharenkov@mail.ru>
2018-05-06 13:00:19 +0300

    test

---
+++ SaltAnalysisData/Analysis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaltAnalysisDatas
+{
+    public abstract class Analysis
+    {
+        public int IDAnalysis { get; set; }
+        public string AnalysisDescription { get; set; }
+        public int IDSample { get; set; }
+        public string LabNumber { get; set; } //just to show in the datagrid
+    }
+}
