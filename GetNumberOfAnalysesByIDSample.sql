f9c94e1083e5eb603fb5ce52e1665601946fa810
ZVV1971 <vvzakharenkov@mail.ru>
2018-07-26 22:38:09 +0300

    Numeric Updown in ContextMenu

--- GetNumberOfAnalysesByIDSample.sql
+++
@@ -1,4 +0,0 @@
-﻿SELECT [Sample].IDSample, COUNT(IDSaltAnalysis) AS c, LabNumber, SamplingDate, [Description]
-FROM [Sample] 
-LEFT JOIN  SaltAnalysis ON [Sample].[IDSample] = [SaltAnalysis].[IDSample]
-GROUP BY [Sample].IDSample, LabNumber, SamplingDate, [Description]
